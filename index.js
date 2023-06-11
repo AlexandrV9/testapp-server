e0657351db8e82e638e32c9563fcc35a77997548
Alexandr <vashchekin01@mail.ru>
2023-06-11 16:30:58 +0300

    refac: add libs

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+
 const app = express();
 const product = require("./api/product");
 
-app.use(express.json({ extended: false }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({ origin: '*' }))
 
 app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
